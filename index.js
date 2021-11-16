e3f68bc2fed541d372a1df0152ba798d0ecf5df7
+ <+>
2021-11-16 09:57:01 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Add your code here
+// // Add your code here
+
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     });
+
+function submitData(userNameInput, userEmailInput){
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userNameInput,
+            email: userEmailInput
+        }),
+      };
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(res => res.json())
+    .then(user => document.body.innerHTML = user.id)
+    .catch(error => document.body.innerHTML = error.message)
+}
+
+
+
+
+// document.querySelector('#users').appendChild(user.id)
